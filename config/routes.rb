4705c4883b75e961bc9657d132db6f4b157f7ea6
Radiet <radiet@41studio.com>
2017-06-04 11:30:50 +0700

    Add detail page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'page#landing'
 
-  get 'page/detail'
+  get 'detail' => 'page#detail'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
